d20480a86b6272176c5d33f85d1cfc88e8e3ab73
barrsofilho <barrosofilho.ti@gmail.com>
2021-11-29 23:17:54 -0300

    remove cloudflare web analytics script

--- pages/components/Footer/index.jsx
+++ pages/components/Footer/index.jsx
@@ -1,7 +1,6 @@
 function Footer(){
     return (
-    <footer>
-        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "782fb46e7a044312a983f7262117a47f"}'></script>    
+    <footer>       
     </footer>  
     )
 }
